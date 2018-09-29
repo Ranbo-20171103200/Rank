7493ffe6625f860491ef81c93b96afe126b74057
Ranbo-20171103200 <1124280263@qq.com>
2018-09-29 09:39:28 +0800

    Sum
    求1到100的和

---
+++ Sum/Sum/Sum/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Sum
+//
+//  Created by s20171103200 on 2018/9/29.
+//  Copyright © 2018年 s20171103200. All rights reserved.
+//
+
+import Foundation
+var sum=0
+for index in 0...100
+{
+    sum+=index
+}
+print(sum)
+
